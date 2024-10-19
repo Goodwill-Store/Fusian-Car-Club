40713de25c4674b8771d69f8d97b71c2ed284639
Anastaisa <anastas.tarasenko001@mymdc.net>
2024-10-19 16:00:09 -0400

    add padding

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,68 +14,69 @@ const Footer = () => {
             sx={{
                 bgcolor: 'primary.main',
                 color: 'white',
-                pt: '3%'
+                pt: '3%',
             }}
         >
-
-            <Grid container spacing={2}>
-                <Grid size={2}>
-                    <List>
-                        <ListItem sx={{ fontWeight: 'bold' }}>Solutions</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Marketing</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Analytics</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Commerce</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Insights</ListItem>
-                    </List>
-                </Grid>
-                <Grid size={2}>
-                    <List>
-                        <ListItem sx={{ fontWeight: 'bold' }}>Pricing</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Documentation</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Guides</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>API Status</ListItem>
-                    </List>
-                </Grid>
-                <Grid size={2}>
-                    <List>
-                        <ListItem sx={{ fontWeight: 'bold' }}>Company</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>About</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Blog</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Jobs</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Press</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Partners</ListItem>
-                    </List>
-                </Grid>
-                <Grid size={2}>
-                    <List>
-                        <ListItem sx={{ fontWeight: 'bold' }}>Legal</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Privacy</ListItem>
-                        <ListItem sx={{ fontWeight: '300' }}>Terms</ListItem>
-                    </List>
-                </Grid>
-                <Grid size={4}>
-                    <List>
-                        <ListItem sx={{ fontWeight: 'bold' }}>
-                            Subscribe to our newsletter
-                        </ListItem>
-                        <ListItem variant="p" gutterBottom>
-                            The latest news, articles, and resources, sent to your inbox weekly.
-                        </ListItem>
-                        <ListItem>
-                            <Grid container spacing={1} alignItems="center">
-                                <Grid item xs={8}>
-                                    <TextField id="outlined-basic" label="Enter your email" variant="filled" fullWidth />
+            <Box sx={{ px: '3%' }}>
+                <Grid container spacing={2}>
+                    <Grid size={2}>
+                        <List>
+                            <ListItem sx={{ fontWeight: 'bold' }}>Solutions</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Marketing</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Analytics</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Commerce</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Insights</ListItem>
+                        </List>
+                    </Grid>
+                    <Grid size={2}>
+                        <List>
+                            <ListItem sx={{ fontWeight: 'bold' }}>Pricing</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Documentation</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Guides</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>API Status</ListItem>
+                        </List>
+                    </Grid>
+                    <Grid size={2}>
+                        <List>
+                            <ListItem sx={{ fontWeight: 'bold' }}>Company</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>About</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Blog</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Jobs</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Press</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Partners</ListItem>
+                        </List>
+                    </Grid>
+                    <Grid size={2}>
+                        <List>
+                            <ListItem sx={{ fontWeight: 'bold' }}>Legal</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Privacy</ListItem>
+                            <ListItem sx={{ fontWeight: '300' }}>Terms</ListItem>
+                        </List>
+                    </Grid>
+                    <Grid size={4}>
+                        <List>
+                            <ListItem sx={{ fontWeight: 'bold' }}>
+                                Subscribe to our newsletter
+                            </ListItem>
+                            <ListItem variant="p" gutterBottom>
+                                The latest news, articles, and resources, sent to your inbox weekly.
+                            </ListItem>
+                            <ListItem>
+                                <Grid container spacing={1} alignItems="center">
+                                    <Grid item xs={8}>
+                                        <TextField id="outlined-basic" label="Enter your email" variant="filled" fullWidth />
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Button variant="contained" color="secondary" sx={{ height: '56px' }}> {/* Adjust this height */}
+                                            Subscribe
+                                        </Button>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={4}>
-                                    <Button variant="contained" color="secondary" sx={{ height: '56px' }}> {/* Adjust this height */}
-                                        Subscribe
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </ListItem>
-                    </List>
+                            </ListItem>
+                        </List>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Box>
             <Divider sx={{ width: '100%', marginBottom: 1, backgroundColor: 'white' }} />
             <Box
                 sx={{
@@ -84,7 +85,9 @@ const Footer = () => {
                     alignItems: 'center',
                     marginTop: 1,
                     height: '100%',
+                    px: '3%'
                 }}
+
             >
                 <Box sx={{ display: 'flex' }}>
                     <Typography variant="body2" sx={{ marginTop: 1 }}>
@@ -120,7 +123,7 @@ const Footer = () => {
                 </Box>
             </Box>
 
-        </Box>
+        </Box >
     );
 };
 
