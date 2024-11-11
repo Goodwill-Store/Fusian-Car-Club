2fd53c82344e7ac7fae1c23e1ca7bb15bd3ed5ba
Anastaisa <anastas.tarasenko001@mymdc.net>
2024-11-11 14:14:14 -0500

    merach api

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const apiRoutes = require('./apiRoutes');
 const userRoutes = require('./userRoutes')
 const eventsRoutes = require('./eventsRoutes')
+const merchRoutes = require('./merchRoutes')
 
 router.use('/', apiRoutes);
 router.use('/user', userRoutes);
 router.use('/events', eventsRoutes);
+router.use('/merch', merchRoutes);
 
 
 module.exports = router;
