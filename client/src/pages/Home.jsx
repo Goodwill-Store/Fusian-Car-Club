ba8c439186c3268ac2b6acf6e88d74c6708f9212
Anastaisa <anastas.tarasenko001@mymdc.net>
2024-11-13 16:18:36 -0500

    add margin

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,19 +28,29 @@ function Home() {
     };
 
     return (
-        <Box
-            sx={{
-                flex: 1,  // Allows Home to expand to fill available space
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                textAlign: 'center',
-            }}
-        >
-            <Hero onGetStartedClick={handleSignUpOpen} onLoginClick={handleLoginOpen} />
-            <SignUpDialog open={isSignUpOpen} onClose={handleSignUpClose} />
-            <LoginDialog open={isLoginOpen} onClose={handleLoginClose} />
+        <Box>
+            <Box
+                sx={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    mt: 8,
+                }}
+
+            >
+                <Hero onGetStartedClick={handleSignUpOpen} onLoginClick={handleLoginOpen} />
+
+                <SignUpDialog open={isSignUpOpen} onClose={handleSignUpClose} />
+                <LoginDialog open={isLoginOpen} onClose={handleLoginClose} />
+
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mt: 2, flexWrap: 'wrap' }}>
+
+                <div class="elfsight-app-e0276fc0-1e71-4190-a7c7-75648ef6d921" data-elfsight-app-lazy></div>
+            </Box>
         </Box>
     );
 }
