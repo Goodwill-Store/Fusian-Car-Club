7fcac0b87ee7f29227be18c00019ea80b013a154
Anastaisa <anastas.tarasenko001@mymdc.net>
2024-11-15 20:13:57 -0500

    fix className

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -6,7 +6,7 @@ const Gallery = ({ urls }) => {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mt: 2, flexWrap: 'wrap' }}>
 
-            <div class="elfsight-app-e0276fc0-1e71-4190-a7c7-75648ef6d921" data-elfsight-app-lazy></div>
+            <div className="elfsight-app-e0276fc0-1e71-4190-a7c7-75648ef6d921" data-elfsight-app-lazy></div>
         </Box>
     );
 };
