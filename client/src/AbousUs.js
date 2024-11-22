c3ee24c2e73ff576acccabeba00373e96e59ed34
DoubleO(n) <160273609+nicolas793685@users.noreply.github.com>
2024-11-22 16:26:32 -0500

    Create AbousUs.js
    About Us page.

---
+++ client/src/AbousUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1> About Us <\h1>
+       <p>Welcome to the Fusian Car Club! We are passionate about cars and the community.</p>
+       <p>Our goal with this website is to create a fun place where car enthusiast and other car event aficionados can find common ground and share their interest and love for cars!</p>
+       <p>Our Team is forged by the best creative minds found in the University of Central Florida.</p>
+ </div>
+  );
+};
+
+export default AboutUs;
+      
